242b3ea7970a22f50d3a78087fcb1b9140741fd9
haechan29 <haechan.im@gmail.com>
2025-08-12 16:41:24 +0900

    필터 UI 관련 로직 변경

--- src/components/FilterItem.tsx
+++ src/components/FilterItem.tsx
@@ -80,16 +80,34 @@ function FilterItem({
       {filters.map((filter, idx) => (
         <div
           key={filter.key}
-          className='flex items-end text-lg font-bold'
+          className='flex text-lg font-bold'
         >
           <button
             onClick={() => {
-              selectKey(filter.key === selectedKey ? undefined : filter.key)}
-            }
+              const wasApplied = selectedValue[filter.key] !== null;
+              const wasSelected = filter.key === selectedKey;
+
+              switch (filter.key) {
+                case 'platform': {
+                  if (wasApplied) {
+                    selectValue({ ...selectedValue, [filter.key]: null });
+                  } else {
+                    selectKey(wasSelected ? undefined : filter.key);
+                  }
+                  break;
+                }
+                case 'experienceLevel': {
+                  const newValue = wasSelected ? null : [0, 10];
+                  selectKey(wasSelected ? undefined : filter.key);
+                  selectValue({ ...selectedValue, [filter.key]: newValue });
+                  break;
+                }
+              }
+            }}
             className={clsx(
               'pr-1 hover:text-blue-500',
               idx === 0 ? '' : 'pl-2',
-              filter.key in selectedValue ? 'text-blue-500' : 'text-gray-400'
+              selectedValue[filter.key] === null ? 'text-gray-400' : 'text-blue-500'
             )}
           >
             {filter.label}
@@ -97,10 +115,20 @@ function FilterItem({
           <FilterOptionItem
             filter={filter}
             isActive={selectedKey === filter.key}
+            setIsActive={(isActive) => selectKey(isActive ? filter.key : undefined)}
             value={selectedValue[filter.key]}
             onChange={(value) => {
-              selectValue({ ...selectedValue, [filter.key]: value});
-              selectKey(selectedKey === undefined ? filter.key : undefined);
+              switch (filter.key) {
+                case 'platform': {
+                  selectValue({ ...selectedValue, [filter.key]: value});
+                  selectKey(selectedKey === undefined ? filter.key : undefined);
+                  break;
+                }
+                case 'experienceLevel': {
+                  selectValue({ ...selectedValue, [filter.key]: value});
+                  break;
+                }
+              }
             }}
             jobPostings={jobPostings}
           />
